96f520bd302e6ce6f7ae00a650d33950b83bd498
Jiří Němec <Nemec.Jiri@stud.vassboskovice.cz>
2022-01-11 11:31:39 +0100

    k

--- Logik/MainWindow.xaml.cs
+++ Logik/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Logik
+namespace Logik_IT1B
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +23,42 @@ namespace Logik
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse ellipse = (Ellipse)sender;
+            if (ellipse.Fill.ToString() == "#FFD9D9D9")
+            {
+                ellipse.Fill = Brushes.Red;
+            }
+            else if (ellipse.Fill == Brushes.Red)
+            {
+                ellipse.Fill = Brushes.Green;
+            }
+            else if (ellipse.Fill == Brushes.Green)
+            {
+                ellipse.Fill = Brushes.Yellow;
+            }
+            else if (ellipse.Fill == Brushes.Yellow)
+            {
+                ellipse.Fill = Brushes.Orange;
+            }
+            else if (ellipse.Fill == Brushes.Orange)
+            {
+                ellipse.Fill = Brushes.Purple;
+            }
+            else if (ellipse.Fill == Brushes.Purple)
+            {
+                ellipse.Fill = Brushes.Blue;
+            }
+            else if (ellipse.Fill == Brushes.Blue)
+            {
+                ellipse.Fill = Brushes.Red;
+            }
+
+
+
         }
     }
 }
